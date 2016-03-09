9677086b5a5bffc6c39aabd6f0926501cc7e4779
Yan <yan@platinum-egg.com>
2016-03-09 21:20:03 +0900

    for new

---
+++ 2DRoguelikeStudy/Assets/Scripts/Mine/BoardManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Yan
+{
+    public class BoardManager : MonoBehaviour
+    {
+        public int rows;                                        //Numbers of row in game board.
+        public int columns;                                     //Numbers of column in game board.
+
+        public GameObject border;                               //Prefab for border.
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
